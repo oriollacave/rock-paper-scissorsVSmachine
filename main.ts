2bbd13da6eb5c70117b4aa7f1c61142d64819037
Oriol Lacave <orioll@gmail.com>
2022-09-22 18:48:31 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,102 @@
+let ma = 0
+let maquina = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    ma = 0
+})
+input.onButtonPressed(Button.B, function () {
+    maquina = randint(1, 3)
+    if (maquina == 1) {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            # # # # #
+            # # # # #
+            `)
+        if (ma == 1) {
+            music.playTone(988, music.beat(BeatFraction.Whole))
+        }
+        if (ma == 2) {
+            music.playTone(131, music.beat(BeatFraction.Whole))
+        }
+        if (ma == 3) {
+            music.playTone(349, music.beat(BeatFraction.Breve))
+        }
+    }
+    if (maquina == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        if (ma == 2) {
+            music.playTone(988, music.beat(BeatFraction.Whole))
+        }
+        if (ma == 3) {
+            music.playTone(131, music.beat(BeatFraction.Whole))
+        }
+        if (ma == 1) {
+            music.playTone(349, music.beat(BeatFraction.Breve))
+        }
+    }
+    if (maquina == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        if (ma == 3) {
+            music.playTone(988, music.beat(BeatFraction.Whole))
+        }
+        if (ma == 1) {
+            music.playTone(131, music.beat(BeatFraction.Whole))
+        }
+        if (ma == 2) {
+            music.playTone(349, music.beat(BeatFraction.Breve))
+        }
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    ma = randint(1, 3)
+    if (ma == 1) {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (ma == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    if (ma == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
